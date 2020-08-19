18a3bd3ca2477644735f7cdc1ea7e88ee60a1d6d
lmejiaf <lmejiaf@uninorte.edu.co>
2020-08-19 17:28:07 -0500

    first commit

---
+++ src/main/java/com/wisedevs/comv/ComvApplication.java
@@ -0,0 +1,13 @@
+package com.wisedevs.comv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wisedevs/comv/ComvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wisedevs.comv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
